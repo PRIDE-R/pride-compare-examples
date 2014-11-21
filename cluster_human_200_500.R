41cb13166eb0c64cb8273f1911a9dcd58f9f28b0
Jose A Dianes <jadianes@gmail.com>
2014-11-21 21:35:46 +0000

    added some clustering samples: scripts + plots

---
+++ cluster_human_200_500.R
@@ -0,0 +1,6 @@
+human.projects.200 <- search.list.ProjectSummary("human", 200)
+human.projects.200.protein.details.500 <- lapply(human.projects.200, function(x) {get.list.ProteinDetail(accession(x), 500)})
+human.clusters.200.500 <- cluster.ProteinDetails(human.projects.200.protein.details.500)
+human.projects.200.accessions <- sapply(human.projects.200, accession)
+plot(human.clusters.200.500, human.projects.200.accessions, main="Clustering of latest 100 human projects")
+
---
+++ cluster_list_1000_100.R
@@ -0,0 +1,5 @@
+projects.1000 <- get.list.ProjectSummary(1000)
+projects.1000.protein.details.100 <- lapply(projects.1000, function(x) {get.list.ProteinDetail(accession(x), 100)})
+clusters.1000.100 <- cluster.ProteinDetails(projects.1000.protein.details.100)
+projects.1000.accessions <- sapply(projects.1000, accession)
+plot(clusters.1000.100, projects.1000.accessions, main="Clustering of latest 1000 projects")
