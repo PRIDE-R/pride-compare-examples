4e66d95578debaf282411c9d6ec511a0d4a2a2c0
Jose A Dianes <jadianes@gmail.com>
2014-11-23 21:05:18 +0000

    added library() calls to scripts
    Archive WS is down today

--- cluster_human_200_500.R
+++ cluster_human_200_500.R
@@ -1,3 +1,6 @@
+library(prider)
+library(prideRcompare)
+
 human.projects.200 <- search.list.ProjectSummary("human", 200)
 human.projects.200.protein.details.500 <- lapply(human.projects.200, function(x) {get.list.ProteinDetail(accession(x), 500)})
 human.clusters.200.500 <- cluster.ProteinDetails(human.projects.200.protein.details.500)
--- cluster_list_1000_100.R
+++ cluster_list_1000_100.R
@@ -1,3 +1,6 @@
+library(prider)
+library(prideRcompare)
+
 projects.1000 <- get.list.ProjectSummary(1000)
 projects.1000.protein.details.100 <- lapply(projects.1000, function(x) {get.list.ProteinDetail(accession(x), 100)})
 clusters.1000.100 <- cluster.ProteinDetails(projects.1000.protein.details.100)
